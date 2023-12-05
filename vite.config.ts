6b3fd8c5b837f6cb72168614a7629338d07b86d5
drumath <2237k.drumath@gmail.com>
2023-12-06 02:40:22 +0900

    :fire: いろいろあって、中身を総とっかえ

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'node:path'
+import { resolve } from "path"
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  if (mode === 'production') {
-    return {
-      plugins: [vue()],
-      resolve: {
-        alias: {
-          '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-      },
-      build: {
-        lib: {
-          entry: resolve(__dirname, 'src/index.ts'),
-          name: 'hello',
-          fileName: 'index'
-        },
-        rollupOptions: {
-          external: ['vue'],
-          output: {
-            globals: {
-              vue: 'Vue'
-            }
-          }
-        }
-      }
-    }
-  }
-
-  // for development
-  return {
-    plugins: [vue()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
+export default defineConfig({
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts")
+      , name: "vue-component-dts",
+      fileName: "index",
+      formats:["es" , "umd"]
     }
   }
 })
