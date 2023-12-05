fd90a3c8eff498f66e50f0710ec364b25f77f26b
drumath <2237k.drumath@gmail.com>
2023-12-06 02:46:09 +0900

    :tada: ビルド時にdtsファイルの生成を確認

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { HelloMessage } from "@/components"
+import { HelloMessage } from "./components"
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path"
-import dts from "vite-plugin-dts"
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts({ exclude: ['src/main.ts'] })],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.ts")
-      , name: "vue-component-dts",
-      fileName: "index",
-      formats:["es" , "umd"]
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-component-dts',
+      fileName: 'index',
+      formats: ['es', 'umd']
     }
   }
 })
