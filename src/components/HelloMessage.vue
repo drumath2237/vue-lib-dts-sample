6f2b519316d7bb18d43920e378d85ff8d7a65cfa
drumath <2237k.drumath@gmail.com>
2023-12-06 00:27:42 +0900

    :sparkles: ファイルの整理　いい感じに

---
+++ src/components/HelloMessage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface Props {
+  name: string
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="hello-container">
+    <p class="hello-message">Hello, {{ props.name }}!!</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.hello-container {
+  border: 2px solid #3e3e3e;
+  border-radius: 2px;
+  width: 300px;
+  text-align: center;
+  margin: 3px;
+
+  .hello-message {
+    font-size: 20px;
+  }
+}
+</style>
